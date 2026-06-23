a074f737bbb3eb1d0c5c4f8c2e05fc54d45fe3bb
jlucascamilo <lucascamilo373@gmail.com>
2026-06-23 16:20:19 -0300

    fix: implementando configuração cors

--- src/main/java/br/com/fiscalizacao/config/SecurityConfig.java
+++ src/main/java/br/com/fiscalizacao/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fiscalizacao.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +25,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Desabilitamos CSRF pois o JWT já protege contra isso
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())// Desabilitamos CSRF pois o JWT já protege contra isso
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         // Rotas exclusivas da Prefeitura
